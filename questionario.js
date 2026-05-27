32b9a3392f0c47dba859215a28ab7d0be28c5b01
luis-cavalheiro <68698834+luis-cavalheiro@users.noreply.github.com>
2026-05-27 16:47:08 -0300

    Night mode start

--- questionario.js
+++ questionario.js
@@ -2,6 +2,7 @@ const NOME_QUESTIONARIO = "ID_questionario";
 const LIMITE_PERGUNTAS = "LIMITEPERGUNTAS";
 const OPCAO_INVERTER = "INVERTERQUESTIONARIO";
 const OPCAO_LIMITAR_TOTAL = "OPCAOLIMITARTOTAL"
+const NIGHT_MODE = "NIGHTMODE"
 //const NOME_QUESTIONARIOS_LISTA = "questionarios";
 
 function aloMundo(){
@@ -127,4 +128,87 @@ function downloadTextFile(text, filename) {
     document.body.removeChild(link);
 
     URL.revokeObjectURL(url); // Release the object URL
+}
+
+
+
+function changeNightMode(){
+    // Pega variavel night mode e muda
+    let nightMode = sessionStorage.getItem(NIGHT_MODE);
+    
+    if (nightMode == null || nightMode == undefined)
+        nightMode = 'false'
+    
+    // Inverter night mode
+    switch(nightMode){
+        case 'true':
+        case true:
+            nightMode = 'false';
+            break;
+        case 'false':
+        case false:
+            nightMode = 'true';
+            break;
+    }
+    
+    console.log(nightMode);
+
+    $("#night-mode-toggle").css('transform', nightMode == 'true' ? 'translateX(-50%)' : 'translateX(-0%)')
+
+    sessionStorage.setItem(NIGHT_MODE, nightMode);
+
+    changeNightModeClasses(nightMode);
+}
+
+function changeNightModeClasses(nightMode){
+    if (nightMode == 'true'){
+        $("html").css("background-color", "rgb(31 31 31)");
+        $(".btn-lista-txt").addClass("night-mode-background");
+        $("h2, .fa-caret-left, .fa-caret-right, label, output, .fa-pen-to-square, #pergunta, #explicaco-pergunta, .btn-lista-txt").addClass("night-mode-text");
+        
+    }
+    else {
+        $("html").css("background-color", "white");
+        $(".btn-lista-txt").removeClass("night-mode-background");
+        $("h2, .fa-caret-left, .fa-caret-right, label, output, .fa-pen-to-square, #pergunta, #explicaco-pergunta, .btn-lista-txt").removeClass("night-mode-text");
+    }
+}
+
+function construirWidget(id){
+    $(`#${id}`).html(`
+        <!-- night mode -->
+        <div style="
+            width: 6.2em;
+            height: 3.5em;
+            position: absolute;
+            overflow: hidden;
+            right: 2%;
+            bottom: 1%;
+            border-radius: 6em;
+            cursor: pointer;
+                ">
+            <div onclick="changeNightMode()" id="night-mode-toggle" style="display: flex; transform: translateX(0%); transition: transform 0.2s;    height: 100%;width: 200%;">
+                <div class="day text-center " style="background: #6579ad;border-radius: 5px;z-index: -1;display:flex;width:100%">
+                    <div class="text-center" style="position:absolute;left: -11%;top: 24%;transform: rotateY(180deg);">
+                        <i class="fa-solid fa-cloud fa-2x" style="color: white;"></i>
+                    </div>
+                    <i class="fa-solid fa-sun" style="color: #f7cb8a;font-size: 2.3em;margin: auto;"></i>
+                </div>
+                
+                
+                <div class="night text-center " style="background: #354265;border-radius: 5px;display:flex; width:100%">
+                    <div class="text-center" style="position:absolute;left: 40%;top: 24%;">
+                        <i class="fa-solid fa-cloud fa-2x" style="color: white;"></i>
+                    </div>
+                        <div class="text-center" style="position:absolute;right: -10%;top: 24%;transform: rotateY(180deg);">
+                        <i class="fa-solid fa-cloud fa-2x" style="color: white;"></i>
+                    </div>
+                    <i class="fa-solid fa-moon fa" style="color: #fff8d6;font-size: 2.3em; margin: auto;"></i>
+                </div>
+            </div>
+        </div>
+        `);
+
+    changeNightMode();
+    changeNightMode();
 }
